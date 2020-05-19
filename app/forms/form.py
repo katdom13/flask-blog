2c9b8c09369a6b9d68ec17a67afdc94bcb9755d7
unknown <katdom13@gmail.com>
2020-05-19 10:17:08 +0800

    Finished login and signup page

--- app/forms/form.py
+++ app/forms/form.py
@@ -23,7 +23,16 @@ from app.models import Account, Post
 class AccountForm(ModelForm):
     class Meta:
         model = Account
-    pass
+
+    first_name = StringField('First name', validators=[DataRequired(), Length(max=50)])
+    last_name = StringField('Last name', validators=[DataRequired(), Length(max=50)])
+    username = StringField('Username', validators=[DataRequired(), Length(max=30), Unique(Account.username, get_session=lambda: db.session)])
+    email = EmailField('Email', validators=[DataRequired(), Length(max=50), Email(), Unique(Account.email, get_session=lambda: db.session)])
+    password = PasswordField('Password', validators=[Optional(), EqualTo('confirm_pass', message='Passwords should match')])
+    confirm_pass = PasswordField('Confirm Password', validators=[Optional(), EqualTo('password', message='Passwords should match')])
+    old_password = PasswordField('Current Password', validators=[EqualTo('confirm_old_pass', message='Passwords should match')])
+    confirm_old_pass = PasswordField('Confirm Current Password', validators=[EqualTo('old_password', message='Passwords should match')])
+
 
 class PostForm(ModelForm):
     class Meta:
